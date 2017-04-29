68f4060510d24f1896b3d4599551bd7b1408a9d4
EugenioGallea <gallea6@gmail.com>
2017-04-29 22:16:13 +0200

    Aggiunta del form della progressione dell'invio del file. Controllo sulle checkbox.

--- ApplicazioneCondivisione/ApplicazioneCondivisione/Form1.Designer.cs
+++ ApplicazioneCondivisione/ApplicazioneCondivisione/Form1.Designer.cs
@@ -40,7 +40,6 @@
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,8 +51,7 @@
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1580, 49);
+            this.menuStrip1.Size = new System.Drawing.Size(1185, 40);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,19 +61,19 @@
             this.connettiToolStripMenuItem,
             this.esciToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(75, 45);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // connettiToolStripMenuItem
             // 
             this.connettiToolStripMenuItem.Name = "connettiToolStripMenuItem";
-            this.connettiToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.connettiToolStripMenuItem.Size = new System.Drawing.Size(222, 38);
             this.connettiToolStripMenuItem.Text = "Connetti...";
             // 
             // esciToolStripMenuItem
             // 
             this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
-            this.esciToolStripMenuItem.Size = new System.Drawing.Size(327, 46);
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(222, 38);
             this.esciToolStripMenuItem.Text = "Esci";
             this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
             // 
@@ -84,22 +82,21 @@
             this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.informazioniSuToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 45);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(38, 36);
             this.toolStripMenuItem1.Text = "?";
             // 
             // informazioniSuToolStripMenuItem
             // 
             this.informazioniSuToolStripMenuItem.Name = "informazioniSuToolStripMenuItem";
-            this.informazioniSuToolStripMenuItem.Size = new System.Drawing.Size(358, 46);
+            this.informazioniSuToolStripMenuItem.Size = new System.Drawing.Size(295, 38);
             this.informazioniSuToolStripMenuItem.Text = "Informazioni su...";
             // 
             // listView1
             // 
             this.listView1.AccessibleName = "UsersList";
-            this.listView1.Location = new System.Drawing.Point(0, 53);
-            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Location = new System.Drawing.Point(0, 43);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1568, 696);
+            this.listView1.Size = new System.Drawing.Size(1177, 562);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
@@ -112,47 +109,35 @@
             // button1
             // 
             this.button1.AccessibleName = "condividiButton";
-            this.button1.Location = new System.Drawing.Point(654, 757);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(490, 610);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(415, 88);
+            this.button1.Size = new System.Drawing.Size(311, 71);
             this.button1.TabIndex = 2;
             this.button1.Text = "Condividi";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.condividionclick);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.AccessibleName = "annullaButton";
-            this.button2.Location = new System.Drawing.Point(1106, 757);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(830, 610);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(415, 88);
+            this.button2.Size = new System.Drawing.Size(311, 71);
             this.button2.TabIndex = 3;
             this.button2.Text = "Annulla";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.labelannulla);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 785);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(565, 29);
-            this.progressBar1.TabIndex = 4;
-            this.progressBar1.Value = 100;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1580, 862);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(1185, 695);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -175,7 +160,6 @@
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
